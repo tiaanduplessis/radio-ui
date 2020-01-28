6fe0ec540363e7c2b56a7f1f93de56913605b3eb
Alastair <alastair@fullfacing.com>
2020-01-28 10:20:36 +0200

    build

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -35691,23 +35691,23 @@ var Select$1 = function Select(_ref3) {
       errors = _useFormContext.errors,
       setValue = _useFormContext.setValue,
       getValues = _useFormContext.getValues,
-      triggerValidation = _useFormContext.triggerValidation; // const getLabel = () => {
-  //   return options && Array.isArray(options) ? options.find(({ value }) => {
-  //     const values = getValues()
-  //     return value === values[name]
-  //   }) : ''
-  // }
-  // const getSelectValue = () => {
-  //   const values = getValues()
-  //   return options.length && getLabel() ? 
-  //   {
-  //     value: values[name],
-  //     label: getLabel().label
-  //   }
-  //   :
-  //   ''
-  // }
+      triggerValidation = _useFormContext.triggerValidation;
 
+  var getLabel = function getLabel() {
+    return options && Array.isArray(options) ? options.find(function (_ref4) {
+      var value = _ref4.value;
+      var values = getValues();
+      return value === values[name];
+    }) : '';
+  };
+
+  var getSelectValue = function getSelectValue() {
+    var values = getValues();
+    return options.length && getLabel() ? {
+      value: values[name],
+      label: getLabel().label
+    } : '';
+  };
 
   return React__default.createElement(InputWrapper, _extends_1({
     alertText: alertTextOverride || (errors[name] ? errors[name].message : ''),
@@ -35751,13 +35751,13 @@ var Select$1 = function Select(_ref3) {
       isMulti: multiple,
       required: required
     }, otherProps)),
-    onChange: function onChange(_ref5) {
-      var _ref6 = slicedToArray(_ref5, 1),
-          selectValue = _ref6[0];
+    onChange: function onChange(_ref6) {
+      var _ref7 = slicedToArray(_ref6, 1),
+          selectValue = _ref7[0];
 
       var value = selectValue.value;
-      setValue(name, multiple ? selectValue.map(function (_ref7) {
-        var itemValue = _ref7.value;
+      setValue(name, multiple ? selectValue.map(function (_ref8) {
+        var itemValue = _ref8.value;
         return itemValue;
       }) : value);
 
