44e43f172a433817c5011294611bd58a13bd8e30
Alastair <alastair@fullfacing.com>
2020-10-14 15:22:14 +0200

    removed unused files

--- src/utils/get-errors.js
+++
@@ -1,5 +0,0 @@
-import getNestedValueFromString from './get-nested-value-from-string'
-
-const getErrors = (errors, name) => getNestedValueFromString(errors, `${name}.message`) || ''
-
-export default getErrors
