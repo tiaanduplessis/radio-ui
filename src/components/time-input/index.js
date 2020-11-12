1ae7eb957b8064c33262b2f9d2fea908d806fa39
Alastair <alastair@fullfacing.com>
2020-11-12 15:06:10 +0200

    updated time input

--- src/components/time-input/index.js
+++ src/components/time-input/index.js
@@ -1,6 +1,34 @@
 import React from "react";
+import { useFormContext } from "react-hook-form";
 import Input from "../input";
 
-const TimeInput = props => <Input {...props} type="time" step="1" />;
+const TimeInput = props => {
+  const { name, onChange = () => {} } = props;
+
+  const { watch, setValue, register } = useFormContext();
+
+  register({ name });
+  const currentValue = watch(name);
+
+  const getTimeString = () =>
+    currentValue ? currentValue.replace("Z", "") : "";
+
+  return (
+    <Input
+      {...props}
+      type="time"
+      step="1"
+      noRef
+      value={getTimeString()}
+      onChange={e => {
+        const { target } = e;
+        const { value } = target;
+
+        setValue(name, `${value}Z`);
+        onChange(e);
+      }}
+    />
+  );
+};
 
 export default TimeInput;
