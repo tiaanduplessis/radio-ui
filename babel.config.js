14f7c86beca627580976d8d0b67110296d938078
Michael <mike.leibbrandt@gmail.com>
2020-12-02 14:52:08 +0200

    fix(storybook build): fixed story-book build/Added TimeInput story
    ASC-1671

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 const sharedPlugins = [
-  "@babel/plugin-proposal-class-properties",
-  "@babel/plugin-proposal-private-methods",
+  ["@babel/plugin-proposal-class-properties", { loose: true }],
+  ["@babel/plugin-proposal-private-methods", { loose: true }],
   "@babel/plugin-proposal-object-rest-spread",
   "@babel/plugin-proposal-optional-chaining",
   "babel-plugin-styled-components"
